c1d4be317705effeabe0101e17023143779fc59d
CodeX1991 <oghenewonaholy@gmail.com>
2024-04-25 09:53:37 +0000

    Implementing catch me

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+export default function handleResponseFromAPI(promise) {
+  return promise.then(() => {
+    console.log('Got a response from the API');
+    return {
+      status: 200,
+      body: 'Success',
+    };
+  }).catch((error) => {
+    console.error('Error:', error.message);
+    return new Error();
+  });
+}
