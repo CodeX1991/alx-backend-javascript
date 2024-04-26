d37119ac599444c8bbd28daff6d9aed4efd4f437
CodeX1991 <oghenewonaholy@gmail.com>
2024-04-26 13:52:17 +0000

    Implementing static method

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,44 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  // Getter and setter for attr: amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  // Getter and setter for attr: currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._currency = newCurrency;
+  }
+
+  // Method
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversinRate !== 'number') {
+      throw new TypeError('amount or conversinoRate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
