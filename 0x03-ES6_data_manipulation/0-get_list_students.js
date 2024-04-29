18229c0ffa6dafb2c64b08fb4fc6adca31329edd
CodeX1991 <oghenewonaholy@gmail.com>
2024-04-29 22:37:50 +0000

    Basic implementation an array

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+    { id: 1, fitstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, fitstname: 'James', location: 'Columbia' },
+    { id: 5, fitstname: 'Serena', location: 'San Francisco' },
+  ];
+}
