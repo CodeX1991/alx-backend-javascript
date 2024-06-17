a2963a96ad7b424b306abe774593be2fe5206663
CodeX1991 <oghenewonaholy@gmail.com>
2024-06-17 10:09:20 +0000

    Correction

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 // Prints in STDOUT the string argument
 function displayMessage(strArg) {
-  process.stdout.write(`${strArg}\n`);
+  console.log(strArg);
 }
 
 // Export the function for use in other files
