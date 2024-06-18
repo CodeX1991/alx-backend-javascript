25230e02f487dff11df9eb8b75eed6b08f5dc936
CodeX1991 <oghenewonaholy@gmail.com>
2024-06-18 11:18:51 +0000

    Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,35 @@
+// Reading a file synchronously with Node JS
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.trim().length > 0);
+
+    if (lines.length <= 1) {
+      throw new Error('Cannot load the database');
+    }
+
+    const students = {};
+    let totalStudents = 0;
+
+    lines.slice(1).forEach((line) => {
+      const [firstname, lastname, age, field] = line.split(',');
+      if (firstname && lastname && age && field) {
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(firstname);
+        totalStudents += 1;
+      }
+    });
+    console.log(`Number of students: ${totalStudents}`);
+    for (const [field, names] of Object.entries(students)) {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
