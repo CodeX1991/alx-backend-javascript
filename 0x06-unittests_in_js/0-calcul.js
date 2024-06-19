58a3bb93514ab7ba70193246cf81ee5a4e503ae5
CodeX1991 <oghenewonaholy@gmail.com>
2024-06-19 23:55:41 +0000

    Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,6 @@
+// A file to calcuulate the sum of two integers
+function calculateNumber(a, b) {
+  return (Math.round(a) + Math.round(b));
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/README.md
@@ -0,0 +1,1 @@
+Learning mocha with Alx
