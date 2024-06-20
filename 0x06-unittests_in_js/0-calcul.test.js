65d635c78cb88f30d607465a718eb028c8f7e9f0
CodeX1991 <oghenewonaholy@gmail.com>
2024-06-20 10:23:21 +0000

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -12,7 +12,13 @@ describe('calculateNumber', () => {
   it('should return 5 for inputs (1.2, 3.7)', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
-  it('should return 6 for inputs (1.5, 3.7)', () => {
+  it('should return 6 for inputs(1.5, 3.7)', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+  it('should return 3 for inputs(1.0, 2.0)', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
+  });
+  it('should return 5 for inputs(2.499999, 3.499999)', () => {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
   });
 });
